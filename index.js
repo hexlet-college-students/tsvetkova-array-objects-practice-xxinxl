4dafcedba787a36103250d136f9d73c25f55d02b
Natalia Lyamina <natalia.lyamina1234@gmail.com>
2024-04-03 23:34:28 +0300

    processTask1

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-// task 1
-const tableParsing = (/* content */) => {
+// const stringToArray = (content) => {
+//     return content.trim().split('\n').map((item) => item.split(';'));
+// };
 
-};
+// const OutputTheAverageRating = (content) => {
+//     const sums = [];
+//     for (let i = 0; i < content.length - 3; i += 1) {
+//         sums.push(content[i + 2] + content[i + 3])
+//     };
+//     console.log(Math.max(...sums))
+// }
 
-// task 2
-const candidateAssessment = (/* content */) => {
 
-};
+// // task 1
+// const tableParsing = (content) => {
+//     const arrayContent = stringToArray(content);
+//     console.log(arrayContent);
+//     OutputTheAverageRating(arrayContent);
+// };
 
-// task 3
-const actorRating = (/* content */) => {
+// // task 2
+// const candidateAssessment = (/* content */) => {
 
-};
+// };
 
-export { tableParsing, candidateAssessment, actorRating };
+// // task 3
+// const actorRating = (/* content */) => {
+
+// };
+
+// export { tableParsing, candidateAssessment, actorRating };
